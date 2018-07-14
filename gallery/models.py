b24fda264f7ef467195802f61f4309dcee5afc93
sigilai <you@example.com>
2018-07-14 23:59:40 +0300

    added models and admin page

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    CATEGORIES =(("SpaceX","SpaceX"),("Blue Origin","Blue Origin"),("Virgin Atlantic","Virgin Atlantic"))
+
+    image_category = models.CharField(max_length=40,choices=CATEGORIES,)
+
+class Location(models.Model):
+    LOCATIONS=(("USA","USA"),("Canada","Canada"),("Britain","Britain"))
+
+    image_location = models.CharField(max_length=40,choices=LOCATIONS,)
+
+class Image(models.Model):
+    image_name = models.CharField(max_length=30)
+    image_description = models.TextField()
+    image_date = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(Category)
+    location = models.ForeignKey(Location)
+
+
+
