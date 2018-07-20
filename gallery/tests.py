81fac16c41750844a561d5f1664382d402ff333b
sigilai <you@example.com>
2018-07-20 12:02:59 +0300

    added test for saving category

--- gallery/tests.py
+++ gallery/tests.py
@@ -16,7 +16,19 @@ class LocationTestClass(TestCase):
         loc = Location.objects.all()
         self.assertTrue(len(loc)>0)
 
+class CategoryTestClass(TestCase):
 
+    def setUp(self):
+        self.space = Category(image_category='SpaceX')
+
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.space,Category))
+
+    def test_save_method(self):
+        self.space.save_category()
+        space = Category.objects.all()
+        self.assertTrue(len(space)>0)
 
 
 
