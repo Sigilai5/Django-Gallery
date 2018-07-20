58cb99a06273f60b6bbf493db4a3ab8d8eca6aa3
sigilai <you@example.com>
2018-07-20 12:19:11 +0300

    added delete function for category

--- gallery/models.py
+++ gallery/models.py
@@ -23,6 +23,10 @@ class Location(models.Model):
 
     def save_location(self):
         self.save()
+
+    def delete_location(self):
+        self.delete()
+
     def __str__(self):
         return self.image_location
 
